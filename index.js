d5c51e62e325022ca2d72e5c59c6a4c0e96d1fc8
Yon Txpmdroflol <jochong27@gmail.com>
2021-09-29 14:23:13 +0200

    2 - Nombre impairs

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //1 - Nombre pairs
-var i 
+var i
+
 for (i = 11; i < 47; i++) {
     if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+//2 - Nombre impairs
+var i
+ 
+for (i = 109; i < 588; i++) {
+    if (i % 2 !== 0) {
         console.log(i)
     }
 }
